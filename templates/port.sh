a3d8cf5f106406b551ae7228679fa0880bc3920c
Eric Fischer <efischer78@yahoo.fr>
2026-03-02 23:03:06 +0100

    ok

--- templates/port.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-find ${CATALINA_HOME}/conf ${CATALINA_HOME}/webapps/docs ${CATALINA_HOME}/webapps/examples -type f \( -name "*.json" -o -name "*.xml" -o -name "*.html" -o -name "*.txt" \) -exec sed -i "s@8080@{{ .httpPort }}@g" {} \;
